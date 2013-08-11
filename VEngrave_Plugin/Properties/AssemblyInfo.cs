27fca02a033e041651b34cb51982b1cdd48ac727
Paul J. Ste. Marie <pstemari@google.com>
2013-08-11 12:01:10 -0700

    Set attributes for b0001

--- VEngrave_Plugin/Properties/AssemblyInfo.cs
+++ VEngrave_Plugin/Properties/AssemblyInfo.cs
@@ -21,12 +21,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VCarve_Plugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VEngrave_Plugin")]
+[assembly: AssemblyDescription("Plugin DLL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VCarve_Plugin")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("V-Engrave Plugin for CamBam")]
+[assembly: AssemblyCopyright("Copyright © 2013 Google.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
